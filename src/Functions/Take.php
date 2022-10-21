7f5f5e50a81b671ead14d9622a2a21ecb7403a12
Jordi Bisbal <jordi.bisbal@gmail.com>
2022-10-21 07:25:31 +0200

    uOptimization on Take

--- src/Functions/Take.php
+++ src/Functions/Take.php
@@ -4,37 +4,37 @@ namespace j45l\functional;
 
 /**
  * @param mixed $target
- * @param string|int|array<string|int> $propertyName
- * @param mixed $defaultValue
+ * @param int|string|array<string|int> $propertyName
+ * @param mixed|null $defaultValue
  * @return mixed|null
  */
-function take($target, $propertyName, $defaultValue = null)
+function take(mixed $target, array|int|string $propertyName, mixed $defaultValue = null): mixed
 {
-    $takeValue = function ($target, $propertyName, $defaultValue) {
-        switch (true) {
-            case is_object($target) && method_exists($target, $propertyName):
-                return $target->$propertyName() ?? $defaultValue;
-            case is_object($target):
-                return $target->$propertyName ?? $defaultValue;
-            case is_array($target):
-                return $target[$propertyName] ?? $defaultValue;
-            default:
-                return $defaultValue;
-        }
+    $takeValue = static function ($target, $propertyName, $defaultValue) {
+        return match (true) {
+            is_object($target) && method_exists($target, $propertyName) =>
+                $target->$propertyName() ?? $defaultValue,
+            is_object($target) =>
+                $target->$propertyName ?? $defaultValue,
+            is_array($target) =>
+                $target[$propertyName] ?? $defaultValue,
+            default =>
+                $defaultValue,
+        };
     };
 
-    switch (true) {
-        case !is_array($propertyName):
-            return $takeValue($target, $propertyName, $defaultValue);
-        case count($propertyName) === 0:
-            return $defaultValue;
-        case count($propertyName) === 1:
-            return take($target, $propertyName[0], $defaultValue);
-        default:
-            return take(
+    return match (true) {
+        !is_array($propertyName) =>
+            $takeValue($target, $propertyName, $defaultValue),
+        count($propertyName) === 0 =>
+            $defaultValue,
+        count($propertyName) === 1 =>
+            take($target, $propertyName[0], $defaultValue),
+        default =>
+            take(
                 take($target, array_slice($propertyName, 0, -1)[0], $defaultValue),
                 array_slice($propertyName, -1),
                 $defaultValue
-            );
-    }
+            ),
+    };
 }
