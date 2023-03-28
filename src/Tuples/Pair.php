ce1b32b2cc5907b2e029283b027d6861767f1385
Jordi Bisbal <jordi.bisbal@gmail.com>
2023-03-28 07:11:02 +0200

    Moved companion factories to the class file itself.

--- src/Tuples/Pair.php
+++ src/Tuples/Pair.php
@@ -7,6 +7,18 @@ namespace j45l\functional\Tuples;
 /**
  * @template T1
  * @template T2
+ * @param T1 $first
+ * @param T2 $second
+ * @return Pair<T1, T2>
+ */
+function Pair(mixed $first, mixed $second): Pair
+{
+    return Pair::from($first, $second);
+}
+
+/**
+ * @template T1
+ * @template T2
  */
 readonly class Pair
 {
--- src/Tuples/Triplet.php
+++ src/Tuples/Triplet.php
@@ -8,6 +8,20 @@ namespace j45l\functional\Tuples;
  * @template T1
  * @template T2
  * @template T3
+ * @param T1 $first
+ * @param T2 $second
+ * @param T3 $third
+ * @return Triplet<T1, T2, T3>
+ */
+function Triplet(mixed $first, mixed $second, mixed $third): Triplet
+{
+    return Triplet::from($first, $second, $third);
+}
+
+/**
+ * @template T1
+ * @template T2
+ * @template T3
  */
 readonly class Triplet
 {
